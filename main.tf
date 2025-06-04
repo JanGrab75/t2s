f55f281b1d91b745804ae4b88f0c790b25053a4a
JanGrab75 <sojuz2000@poczta.fm>
2025-06-04 10:12:18 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-ArkadiuszK
+    storage_account_name = "starkadiusk"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "arkadiusK-app-service-plan"
+  location            = "westeurope"
+  resource_group_name = "rg-ArkadiuszK"
+  os_type             = "Linux"
+  sku_name            = "P0v3"
+}
+
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "arkadiuszk-webapp-t2s-workshop-1"
+  location            = "westeurope"
+  resource_group_name = "rg-ArkadiuszK"
+  service_plan_id     = azurerm_service_plan.example.id
+  site_config {}
+}
