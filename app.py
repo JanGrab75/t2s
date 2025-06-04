c3aff802e38abceaf204a16ff5059af8587a9d2c
JanGrab75 <sojuz2000@poczta.fm>
2025-06-04 13:06:21 +0000

    c9

--- app.py
+++ app.py
@@ -4,7 +4,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello & Bye"
+    return '''
+    <html>
+      <head>
+        <title>Hello & Bye@2</title>
+      </head>
+      <body>
+        <h1>Hello & Bye@2</h1>
+        <button onclick="showTime()">Pokaż godzinę</button>
+        <p id="time"></p>
+        <script>
+          function showTime() {
+            var now = new Date();
+            document.getElementById('time').innerText = now.toLocaleTimeString();
+          }
+        </script>
+      </body>
+    </html>
+    '''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
